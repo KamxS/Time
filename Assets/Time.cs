377aa6805e29c5dc0001a3f8e3cb70c11f1898be
MasnyKurwaBen <multi9konto@outlook.com>
2024-03-15 17:08:35 +0100

    czas

---
+++ Assets/Time.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Time : MonoBehaviour
+{
+    public Image uiFill;
+    public float time = 1f;
+    public float loseTimeFloat = 1f;
+
+    public bool slowTime;
+
+
+    private void Update()
+    {
+
+        if(time > 1f)
+        {
+            time = 1f;
+        }
+
+        if (time < 0f)
+        {
+            time = 0f;
+        }
+
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            slowTime = !slowTime;
+        }
+
+        if(slowTime)
+        {
+            time -= 0.0005f;
+            uiFill.fillAmount = time;
+        }
+        else
+        {
+            time += 0.0005f;
+            uiFill.fillAmount = time;
+        }
+    }
+
+
+    void loseTime(float loseTimeFloat)
+    {
+        time -= 0.5f;
+        uiFill.fillAmount = time;
+    }
+
+   void GainTime(float loseTimeFloat)
+   {
+        time += 0.5f;
+        uiFill.fillAmount = time;
+    }
+
+
+
+}
