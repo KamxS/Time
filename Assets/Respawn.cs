4eb0ebea7cc84e9b20ee3222ebce1f6a053fef14
Kamx <kamxsyt@gmail.com>
2024-03-16 10:59:51 +0100

    KONIEC.

--- Assets/Respawn.cs
+++ Assets/Respawn.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -7,6 +8,11 @@ public class Respawn : MonoBehaviour
 {
     public Player player;
     public GameObject gameoverUI;
+    public GameObject timeScale;
+    public GameObject comboScale;
+    public GameObject levelScale;
+    public TextMeshProUGUI scoreTxt;
+    public TextMeshProUGUI highscoreTxt;
 
     private void Start()
     {
@@ -18,13 +24,16 @@ public class Respawn : MonoBehaviour
     {
         if (Player.playerDie)
         {
+                timeScale.SetActive(false);
+                comboScale.SetActive(false);
+                levelScale.SetActive(false);
             ComboSystem combo = GameObject.FindGameObjectWithTag("Spawn").GetComponent<ComboSystem>();
             if(combo.points > PlayerPrefs.GetInt("Highscore",0))
             {
                 PlayerPrefs.SetInt("Highscore", combo.points);
             }
-            Debug.Log("Score: " + combo.points);
-            Debug.Log("HighScore: " + PlayerPrefs.GetInt("Highscore",0));
+            scoreTxt.text = "Your Score is: " + combo.points.ToString();
+            highscoreTxt.text = "Your HighScore is: " + PlayerPrefs.GetInt("Highscore",0);
             gameoverUI.SetActive(true);
 
             if (Input.GetKeyDown(KeyCode.R))
